c2cf65bff43713c28275a6ce7098f8433a608ab4
perqin <perqinxie@gmail.com>
2017-06-09 12:18:23 +0800

    Change port to 5000 in CI

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 export API_APPKEY=$(cat /var/www/eatpotmovie-tca-ci/appkey)
 
-uwsgi --http 127.0.0.1:7575 --manage-script-name --mount /=manage:app --virtualenv ./venv/
+uwsgi --http 127.0.0.1:5000 --manage-script-name --mount /=manage:app --virtualenv ./venv/
 
 exit 0
